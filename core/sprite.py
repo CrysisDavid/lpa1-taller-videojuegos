97c292aa2f131bfc0e57fd7fa004bc61a069fad4
david <davidsoliss123@gmail.com>
2026-04-17 17:54:26 -0500

    wip

--- core/sprite.py
+++ core/sprite.py
@@ -1,9 +1,11 @@
 from abc import ABC
 import pygame
 
+from core.vector2d import Vector2D
+
 class Sprite(ABC):
     def __init__(self, screen: pygame.Surface, x:float,y:float, color: tuple, raius: int):
         self.screen = screen
-        # self.position = Vector2D(x,y)
+        self.position = Vector2D(x,y)
         self.color = color
         self.radius = raius
